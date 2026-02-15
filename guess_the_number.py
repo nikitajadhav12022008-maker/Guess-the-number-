80e9a32a244073f0d96c0d4c0e3c0c337484f9e8
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-15 01:12:05 -0800

    code updated

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,10 +1,12 @@
 #GUESS THE NUMBER:
+#importing random:
 import random
 target = random.randint(1,100)
 
-
+#using loops:
 while True:
     userchoice = int(input("enter the number or QUIT(Q):"))
+    #using conditional statements for checking whether the target is greater or smaller:
     if(userchoice == "Q"):
         break
 
@@ -16,7 +18,7 @@ while True:
         print("number is too small take a bigger guess......")
     else:
         print("number is too big take a smaller guess...")
-
+#if guessed is correct print game over:
 print("GAME OVER!!")
 
 
@@ -25,6 +27,7 @@ print("GAME OVER!!")
 
 
     
+
 
 
 
